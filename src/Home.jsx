2ad337bec665b446e1d31f18a151a78a7dc8179e
Ben Taitelbaum <ben@coshx.com>
2017-10-21 15:18:20 -0700

    adds routes and links
     - switches to using .jsx file extension for react jsx files
     - moves components into separate files
     - leaves example of bindingof pushing app state into another component

---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+
+class Home extends Component {
+  render() {
+    return (
+        <div className="pure-g">
+          <div className="pure-u-1-1">
+            <h1>Smart & Safe Credit Reports</h1>
+            <p>Luminiferous takes the mystery out of credit reporting, both for consumers as well as lenders.</p>
+            <h2>Check My Credit Report</h2>
+            <Link className="pure-button pure-button-primary" to="/credit-report">Get Report</Link>
+            <h2>Check Someone Else's Credit Report</h2>
+          </div>
+        </div>
+    );
+  };
+}
+
+export default Home
