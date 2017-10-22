cf02f04288efbcf134cdf479743a39284f71abbf
Michael Holroyd <meekohi@gmail.com>
2017-10-22 09:00:46 -0700

    logs

--- src/TransactionList.jsx
+++ src/TransactionList.jsx
@@ -12,14 +12,35 @@ class TransactionList extends LumiComponent {
   lumiInit() {
     var borrowed = this.state.lumi.borrowed({}, {fromBlock: 0, toBlock: 'latest'});
     borrowed.get( (err, logs) => {
+      var that = this;
       this.setState({logs: logs});
       console.dir(logs);
 
-      // mocked up (for now)
-      this.setState({logs: [
-        {description: 'Credit Card Payment 10/16', amount: '$204.12 = 0.686 ETH'},
-        {description: 'Credit Card Payment 10/1', amount: '$1672.34 = 5.616 ETH'}
-      ]});
+      var human_logs = logs.map(function(log){
+        var bal = log.args.amount.toNumber();
+        var t = new Date(log.args.timestamp.toNumber()*1000);
+        console.log(t);
+        if(log.event === "borrowed") {
+          return {
+            description: "Credit Card Payment ... " + t.toLocaleString(),
+            amount: that.state.web3.fromWei(bal, 'ether')+" ETH"
+          };
+        } else {
+          return {
+            description: "Repayment ...(date)",
+            amount: that.state.web3.fromWei(bal, 'ether')+" ETH"
+          };
+        }
+
+      });
+
+      // var human_logs = [
+      //   {description: 'Credit Card Payment 10/16', amount: '$204.12 = 0.686 ETH'},
+      //   {description: 'Credit Card Payment 10/1', amount: '$1672.34 = 5.616 ETH'}
+      // ];
+
+      console.log(human_logs);
+      this.setState({logs: human_logs});
 
     });
 
