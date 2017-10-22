c926e16967e6570fe9b198d3c376e71d4a7db93f
Ben Taitelbaum <ben@coshx.com>
2017-10-22 06:50:00 -0700

    styles transaction list

--- src/OfferList.jsx
+++ src/OfferList.jsx
@@ -8,6 +8,28 @@ class OfferList extends Component {
     this.state = { offers: props.offers };
   }
 
+  componentWillMount() {
+    console.dir(process.env);
+    console.log('ID: ' + process.env.CAPITALONE_CLIENT_ID);
+    fetch('https://api-sandbox.capitalone.com/oauth2/token',
+          {
+            method: 'post',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'client_id=' + process.env.CAPITALONE_CLIENT_ID
+              + '&client_secret=' + process.env.CAPITALONE_CLIENT_SECRET
+          })
+      .then( (data) => {
+        console.log("Got token!");
+        console.dir(data);
+      })
+      .catch( (err) => {
+        console.log("Error fetching auth token", err);
+      });
+  }
+
   render() {
     var offersList = this.state.offers.map( (offer) => {
       return <Offer name={offer.name} terms={offer.terms} key={offer.id} id={offer.id}/>;
--- src/TransactionList.jsx
+++ src/TransactionList.jsx
@@ -4,10 +4,36 @@ import LumiComponent from './LumiComponent';
 
 class TransactionList extends LumiComponent {
 
+  constructor(props) {
+    super(props);
+
+    this.state.logs = [];
+  }
   lumiInit() {
+    var borrowed = this.state.lumi.borrowed({}, {fromBlock: 0, toBlock: 'latest'});
+    borrowed.get( (err, logs) => {
+      this.setState({logs: logs});
+      console.dir(logs);
+
+      // mocked up (for now)
+      this.setState({logs: [
+        {description: 'Credit Card Payment 10/16', amount: '$204.12 = 0.686 ETH'},
+        {description: 'Credit Card Payment 10/1', amount: '$1672.34 = 5.616 ETH'}
+      ]});
+
+    });
+
   }
 
   render() {
+    var txList = this.state.logs.map( (log) => {
+      return (
+        <div className="transaction">
+          <span className="description">{log.description}</span>
+          <span className="amount">{log.amount}</span>
+        </div>
+      );
+    });
     return (
       <div className="TransactionList DashboardWidget pure-u-1-1">
         <div className="header">
@@ -17,6 +43,7 @@ class TransactionList extends LumiComponent {
           </Link>
         </div>
         <div className="transaction">
+          {txList}
         </div>
       </div>
     );
