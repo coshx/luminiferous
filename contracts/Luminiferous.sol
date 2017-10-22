131f9b935524e529250743944d3d1d165dc3ec82
Michael Holroyd <meekohi@gmail.com>
2017-10-21 19:41:01 -0700

    fucking crap

--- contracts/Luminiferous.sol
+++ contracts/Luminiferous.sol
@@ -22,34 +22,10 @@ contract Luminiferous {
     interest_updated_at = block.timestamp;
   }
 
-  // Helper functions
-  function isSigned() returns (bool) {
-    return signed && (borrower > 0);
-  }
-  function isLender(address _addr) returns (bool) {
-    return address(lender) == _addr;
-  }
-  function isBorrower(address _addr) returns (bool) {
-    return borrower == _addr;
-  }
-
-  // Modifiers
-  modifier onlysigned {
-    require(isSigned());
-    _;
-  }
-  modifier onlyunsigned {
-    require(!isSigned());
-    _;
-  }
-  modifier onlyborrower {
-    require(isBorrower(msg.sender));
-    _;
-  }
-  modifier onlylender {
-    require(isLender(msg.sender));
-    _;
-  }
+  event ContractUnsignedError();
+  event ContractAlreadySignedError();
+  event BorrowerAuthError();
+  event RequestFunds(uint amount);
 
   // Fallback function!
   // remember: 2300 maximum gas
@@ -58,40 +34,55 @@ contract Luminiferous {
   }
 
   // Step 0. The Lender sets the terms (currently hard-coded for testing and demo)
-  function setLumibank(address bank) onlyunsigned external {
+  function setLumibank(address bank) external {
+    if(signed) {ContractAlreadySignedError(); return;}
     lender = LuminiferousBank(bank);
   }
 
   // Step 1. The Lender approves a specific borrower after checking their credit score
-  function approve_borrower(address _borrower) onlyunsigned onlylender external {
+  function approve_borrower(address _borrower) external {
+    if(signed) {ContractAlreadySignedError(); return;}
+    if(msg.sender != address(lender)) {return;}
     borrower = _borrower;
   }
 
   // Step 2. The Borrower reviews the contract, agrees to the terms, signs the contract
-  function sign_contract() onlyunsigned onlyborrower external {
+  function sign_contract() external {
+    if(msg.sender != borrower) { BorrowerAuthError(); return;}
+    if(signed) {ContractAlreadySignedError(); return;}
     signed = true;
   }
 
   // Step 3. The Borrower asks the Lender to put money into the contract
-  function updateCreditLimit(uint new_limit) onlysigned onlyborrower external {
+  function updateCreditLimit(uint new_limit) external {
+    if(!signed) {ContractUnsignedError(); return;}
+    if(msg.sender != borrower) { BorrowerAuthError(); return;}
     if(borrower_balance > requested_credit_limit){ return; } // can't set the credit limit below your current balance
     requested_credit_limit = new_limit;
+    RequestFunds(requested_credit_limit - borrower_balance);
     lender.request_funds(requested_credit_limit - borrower_balance); // Ask CapitalOne for funds
   }
 
   // Lender calls this to deposit the funds. Can't deposit more than the borrower asked for.
-  function accept_funds() payable onlysigned onlylender external {
+  function accept_funds() payable external {
+    if(!signed) {ContractUnsignedError(); return;}
+    if(msg.sender != address(lender)) {return;}
+    // TODO: check that this only goes up to requested limit
     borrower_balance += msg.value; // funds are now available in the account
   }
 
   // Step 4. The Borrower can withdraw all funds into their personal account and spend them.
   //         note that unlike a normal contract, we don't need withdraw pattern here (only borrower can withdraw via this function)
-  function withdraw() onlysigned onlyborrower external {
+  function withdraw() external {
+    if(!signed) {ContractUnsignedError(); return;}
+    if(msg.sender != borrower) { BorrowerAuthError(); return;}
     borrower.transfer(this.balance); 
   }
 
   // Step 5. Lender pings the contract to compute interest on the loan
-  function compute_interest() onlysigned onlylender external {
+  function compute_interest() external {
+    if(!signed) {ContractUnsignedError(); return;}
+    if(msg.sender != address(lender)) {return;}
     uint interest_period = block.timestamp - interest_updated_at; // in seconds
     borrower_balance = borrower_balance + (borrower_balance * interest_period / eth_second_per_interest); //overflow risk?
     interest_updated_at = block.timestamp;
@@ -99,7 +90,9 @@ contract Luminiferous {
 
   // Step 6. Borrower makes a payment on the loan. Note that because this is payable you can
   //         send money along with it, which will update `this.balance`.
-  function repay(bool reset_limit) payable onlysigned onlyborrower external {
+  function repay(bool reset_limit) payable external {
+    if(!signed) {ContractUnsignedError(); return;}
+    if(msg.sender != borrower) { BorrowerAuthError(); return;}
     uint repayment_amount = this.balance;
     bool result = lender.return_funds.value(repayment_amount)();
     if(result) {
--- test/TestLumi.js
+++ test/TestLumi.js
@@ -13,14 +13,61 @@ contract('Luminiferous', function(accounts) {
       lumibank = _lumibank;
       return Luminiferous.new();
     }).then(function(lumi){
-      return lumi.setLumibank.call(lumibank.address);
+      return lumi.setLumibank(lumibank.address);
     }).then(function(){
       return web3.eth.sendTransaction({from:capitalone, to:lumibank.address, value:100});
     }).then(function(){
       return web3.eth.getBalance(lumibank.address);
     }).then(function(bal){
-      console.log(bal);
       assert.equal(bal,100);
+    });
+  });
+
+  it("...computing interest on 0 ETH does nothing", function() {
+    var lumibank;
+    var lumi;
+    return LuminiferousBank.deployed().then(function(_lumibank){
+      lumibank = _lumibank;
+      return Luminiferous.new();
+    }).then(function(_lumi){
+      lumi = _lumi;
+      return lumi.setLumibank.call(lumibank.address);
+    }).then(function(){
+      return web3.eth.sendTransaction({from:capitalone, to:lumibank.address, value:200});
+    }).then(function(){
+      return lumi.approve_borrower.call(michael, {from:lumibank.address});
+    }).then(function(){
+      return lumi.sign_contract.call({from:michael});
+    }).then(function(){
+      return lumi.compute_interest.call({from:lumibank.address});
+    }).then(function(){
+      return web3.eth.getBalance(lumi.address);
+    }).then(function(bal){
+      assert.equal(0,0);
+    });
+  });
+
+  it("...michael spends 12ETH", function() {
+    var lumibank;
+    var lumi;
+    return LuminiferousBank.deployed().then(function(_lumibank){
+      lumibank = _lumibank;
+      return Luminiferous.new();
+    }).then(function(_lumi){
+      lumi = _lumi;
+      return lumi.setLumibank.call(lumibank.address);
+    }).then(function(){
+      return web3.eth.sendTransaction({from:capitalone, to:lumibank.address, value:200});
+    }).then(function(){
+      return lumi.approve_borrower.call(michael, {from:lumibank.address});
+    }).then(function(){
+      return lumi.sign_contract.call({from:michael});
+    }).then(function(){
+      return lumi.updateCreditLimit.call(12, {from:michael});
+    }).then(function(){
+      return web3.eth.getBalance(lumi.address);
+    }).then(function(bal){
+      assert.equal(0,12);
     });
   });
 
